c63cd3374fc1d737167eaef3717e7a5cf1631951
orboitiiii <kiwi999990@gmail.com>
2025-11-21 23:50:10 +0800

    del

--- Main/src/main/java/frc/robot/Robot.java
+++ Main/src/main/java/frc/robot/Robot.java
@@ -1,16 +1,8 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
-import frc.robot.lib.core.RobotIntentState;
-import frc.robot.lib.core.RobotOperatingContext;
-import frc.robot.subsystems.SubsystemManager;
 
 public class Robot extends TimedRobot {
-
-  private final SubsystemManager subs = new SubsystemManager();
-  public RobotOperatingContext robotContext;
-  public RobotIntentState desired;
 
   @Override
   public void robotInit() {
@@ -18,40 +10,17 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void robotPeriodic() {
-    // Update Sensors & Odometry (State Estimation)
-    subs.periodic();
-
-    // Read Inputs & Context (Perception)
-    robotContext = subs.readContext();
-  }
+  public void robotPeriodic() {}
 
   @Override
-  public void teleopInit() {
-    desired = decideRobotState(robotContext);
-  }
+  public void teleopInit() {}
 
   @Override
-  public void teleopPeriodic() {
-    // Determine Intent (Planning)
-    desired = decideRobotState(robotContext);
-
-    // Execute Subsystems (Control)
-    subs.operate(desired, robotContext);
-  }
+  public void teleopPeriodic() {}
 
   @Override
-  public void disabledInit() {
-    desired = RobotIntentState.DISABLED;
-  }
+  public void disabledInit() {}
 
   @Override
-  public void disabledPeriodic() {
-    subs.operate(desired, robotContext);
-  }
-
-  public RobotIntentState decideRobotState(RobotOperatingContext ctx) {
-    if (DriverStation.isDisabled()) return RobotIntentState.DISABLED;
-    return RobotIntentState.TRAVEL;
-  }
+  public void disabledPeriodic() {}
 }
--- Main/src/main/java/frc/robot/lib/core/RobotIntentState.java
+++
@@ -1,6 +0,0 @@
-package frc.robot.lib.core;
-
-public enum RobotIntentState {
-  DISABLED,
-  TRAVEL
-}
